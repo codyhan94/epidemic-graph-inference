aa5eec5d25d6e77f61e82add2479ebca02046d05
Cody Han <codyhan94@gmail.com>
2015-04-20 04:33:16 -0700

    Graph loading capability in SIsim.

--- graph_inference/sim/sisim.py
+++ graph_inference/sim/sisim.py
@@ -1,24 +1,34 @@
 from graph_inference.sim.basesim import BaseSim
 from graph_inference.graphs import basegraph
 
+import random
+
 import numpy as np
 import networkx as nx
 
 class SISim(BaseSim):
     """Base class for network inference simulations."""
 
-    def __init__(self, graph, n_cascades):
+    def __init__(self, graph_file, n_cascades, mu=0.1):
         """Set up a
         """
-        self.graph = graph
+        self.G = nx.read_graphml(graph_file)
         self.n_cascades = n_cascades
+        self.mu = mu
+
+        self.initialize_graph()
         pass
+
+    def initialize_graph(self):
+        """Set up the graph.."""
+        return
 
     def step(self):
         """Runs one step of the simulation.
 
         :return: Nothing
         """
+
         return
 
     def run(self):
