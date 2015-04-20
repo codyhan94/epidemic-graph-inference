bdad88e33db2662ed6fec7eb7967388e5076c2b3
Kevin Tang <ktang@caltech.edu>
2015-04-20 03:31:51 -0700

    Added a way to simply run the code. Not quite working yet

--- graph_inference/graphs/gnp.py
+++ graph_inference/graphs/gnp.py
@@ -1,6 +1,7 @@
 from graph_inference.graphs.basegraph import BaseGraph
 
 import networkx as nx
+import sys
 
 class GNPgraph(BaseGraph):
     """Generates a GNP graph."""
@@ -8,6 +9,14 @@ class GNPgraph(BaseGraph):
     def __init__(self, arg):
         super(GNPgraph, self).__init__()
         self.arg = arg
+        self.generate()
 
     def generate(self, n=1000, p=.5, directed=False):
         self.G = nx.fast_gnp_random_graph(n, p, None, directed)
+
+if __name__ == "__main__":
+    """ Example code """
+    filename = sys.argv[1]
+    graph = GNPgraph()
+    graph.graphml(filename)
+
