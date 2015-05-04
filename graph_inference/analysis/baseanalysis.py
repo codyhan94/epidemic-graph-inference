8e98a0c3e4cb377de16b42cb3e7f0dbb39c42fa4
Kevin Tang <ktang@caltech.edu>
2015-05-04 06:54:11 -0700

    fixed bugs and improved the generaltest.py

--- graph_inference/analysis/baseanalysis.py
+++ graph_inference/analysis/baseanalysis.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 import networkx as nx
+import matplotlib.pyplot as plt
+import math
 
 
 class BaseAnalysis(object):
@@ -12,7 +14,29 @@ class BaseAnalysis(object):
         self.G = nx.readwrite.graphml.read_graphml(graphfile)
         self.H = nx.readwrite.graphml.read_graphml(inferredfile)
 
-    def edgeerror(self, G=None, H=None):
+    def edgeCorrect(self, G=None, H=None):
+        """
+        Returns the number of edges in the original graph that are
+        correctly predicted
+
+        :param G: networkx graph of original graph (default: self.G)
+        :param H: networkx graph of inferred graph (default: self.H)
+        :return: int
+        """
+        if G is None:
+            G = self.G
+        if H is None:
+            H = self.H
+
+        gedges = G.edges()
+        hedges = H.edges()
+        count = 0
+        for gedge in gedges:
+            if gedge in hedges:
+                count += 1
+        return count
+
+    def edgeError(self, G=None, H=None):
         """
         Returns the number of edges in the original graph that aren't
         in the inferred graph.
@@ -33,7 +57,7 @@ class BaseAnalysis(object):
                 count += 1
         return count
 
-    def edgeextra(self, G=None, H=None):
+    def edgeExtra(self, G=None, H=None):
         """
         Returns the number of extraneous edges the inferred graph has.
 
@@ -46,9 +70,9 @@ class BaseAnalysis(object):
         if H is None:
             H = self.H
 
-        return self.edgeerror(H, G)
+        return self.edgeError(H, G)
 
-    def edgedifference(self, G=None, H=None):
+    def edgeDifference(self, G=None, H=None):
         """ Returns the difference in number of edges between the two graphs.
 
         :param G: networkx graph of original graph (default: self.G)
@@ -61,7 +85,7 @@ class BaseAnalysis(object):
             H = self.H
         return G.number_of_edges() - H.number_of_edges()
 
-    def neighbormatching(self, G=None, H=None):
+    def neighborMatching(self, G=None, H=None):
         """ Returns the graph similarity based on Neighbor Matching[Ref]
         Derived from 'wadsashika'_
 
@@ -89,7 +113,7 @@ class BaseAnalysis(object):
 
         # TODO
 
-    def degreesequence(self, G=None, H=None):
+    def degreeSequence(self, G=None, H=None):
         """ Returns how similar two graphs are by comparing their degree sequence.
         The sum of scaled things
         of ratio of
@@ -106,12 +130,12 @@ class BaseAnalysis(object):
         gdegree = sorted(nx.degree(G).values(), reverse=True)
         hdegree = sorted(nx.degree(H).values(), reverse=True)
 
-        degreelist = [self.__degreesequencehelper(x, y) for x, y
+        degreelist = [self.__degreeSequenceHelper(x, y) for x, y
                       in zip(gdegree, hdegree)]
 
         return sum(degreelist) / len(degreelist)
 
-    def nodedegreedifference(self, G=None, H=None):
+    def nodeDegreeDifference(self, G=None, H=None):
         """ Returns how similar two graphs are by comparing the degree of each
         corresponding node
 
@@ -126,12 +150,12 @@ class BaseAnalysis(object):
 
         gdegree = nx.degree(G).values()
         hdegree = nx.degree(H).values()
-        degreelist = [self.__degreesequencehelper(x, y) for x, y
+        degreelist = [self.__degreeSequenceHelper(x, y) for x, y
                       in zip(gdegree, hdegree)]
 
         return sum(degreelist) / len(degreelist)
 
-    def __degreesequencehelper(self, i, j):
+    def __degreeSequenceHelper(self, i, j):
         """ Helper function for degree sequence """
         if i is None or i == 0:
             return float(j)
@@ -139,3 +163,49 @@ class BaseAnalysis(object):
             return float(i)
         else:
             return float(abs(i - j)) / i
+
+    # Plotting functions!
+    def plotGraphs(self, G, H):
+        if G is None:
+            G = self.G
+        if H is None:
+            H = self.H
+
+        plt.figure()
+        plt.title('original graph')
+        nx.draw(G, self.__circlepos(G))
+        plt.figure()
+        plt.title('analyzed graph, with %d' % 5)
+        nx.draw(H, self.__circlepos(H))
+
+    # def plothistogram:
+    #     """ Sourced from https://networkx.github.io/documentation/latest/
+    #     examples/drawing/degree_histogram.html"""
+
+    #     degree_sequence=sorted(nx.degree(G).values(),
+    #                            reverse=True)  # degree sequence
+    #     #print "Degree sequence", degree_sequence
+    #     dmax=max(degree_sequence)
+
+    #     plt.loglog(degree_sequence,'b-',marker='o')
+    #     plt.title("Degree rank plot")
+    #     plt.ylabel("degree")
+    #     plt.xlabel("rank")
+
+    #     # draw graph in inset
+    #     plt.axes([0.45,0.45,0.45,0.45])
+    #     Gcc=sorted(nx.connected_component_subgraphs(G), key = len, reverse=True)[0]
+    #     pos=nx.spring_layout(Gcc)
+    #     plt.axis('off')
+    #     nx.draw_networkx_nodes(Gcc,pos,node_size=20)
+    #     nx.draw_networkx_edges(Gcc,pos,alpha=0.4)
+
+    def __circlePos(self, G, r0=10):
+        pos = {}
+        d = max(G.degree().values())
+        n = G.number_of_nodes()
+        for node in G.nodes():
+            deg = 2 * math.pi * int(node) / n
+            r = r0 * (1 - G.degree(node) / d) + .5
+            pos[node] = (r * math.sin(deg), r * math.cos(deg))
+        return pos
