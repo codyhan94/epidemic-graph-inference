0f56bcb18cf5cbc68c24124667a0265b59a63bd4
Cody Han <codyhan94@gmail.com>
2015-05-16 07:39:15 -0700

    Added three node tests to greedy solver tests.

--- tests/sim/test_greedysolver.py
+++ tests/sim/test_greedysolver.py
@@ -1,3 +1,4 @@
+from pdb import set_trace
 import unittest
 import numpy as np
 
@@ -35,10 +36,52 @@ class TestGreedySolverMethods(unittest.TestCase):
 
     def test_three_nodes_line(self):
         """
-        Tests one cascade on three nodes in a line graph.
+        Tests mutiple cascades on three nodes in a line graph.
 
         Graph:
             0 -> 1 -> 2
         """
-        cascades = np.array([])
-        pass
+        cascades = np.array([[0, 1, 2]])
+        solver = GreedySolver(cascades)
+
+        # Test that each node identifies its parental neighborhood correctly
+        # when 0 is the seed node.
+        self.assertEqual(solver.solve_node(0), [])
+        self.assertEqual(solver.solve_node(1), [0])
+        self.assertEqual(solver.solve_node(2), [1])
+
+        # Test the same as above but with 2 as the seed node, changing the
+        # direction of the above arrows.
+        solver.cascades = np.array([[2, 1, 0]])
+        self.assertEqual(solver.solve_node(0), [1])
+        self.assertEqual(solver.solve_node(1), [2])
+        self.assertEqual(solver.solve_node(2), [])
+
+        # Test having the middle node as the seed.
+        solver.cascades = np.array([[1, 0, 1]])
+        self.assertEqual(solver.solve_node(0), [1])
+        self.assertEqual(solver.solve_node(1), [])
+        self.assertEqual(solver.solve_node(2), [1])
+
+    def test_three_nodes_circle(self):
+        """
+        Tests multiple cascades on three nodes.
+
+        Graph:
+            0 _ 1
+             \ /
+              2
+        """
+
+        # Two cascades with 0 as the seed, one where all nodes get infected
+        # and one where only node 1 gets infected. Similarly, two cascades
+        # with 1 as a seed, one where all nodes get infected and one where
+        # only 0 gets infected.
+        cascades = np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0], [0, 1, np.inf],
+            [1, 0, np.inf]])
+        solver = GreedySolver(cascades)
+
+        self.assertEqual(solver.solve_node(0), [1])
+        self.assertEqual(solver.solve_node(1), [0])
+        # set_trace()
+        # solver.solve_node(2)
