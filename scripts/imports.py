ded9139292b6dbaf8ed3ebc5ff5337c434edab76
Cody Han <codyhan94@gmail.com>
2015-05-19 00:03:24 -0700

    Added an imports file to be loaded with ipython during testing.

---
+++ scripts/imports.py
@@ -0,0 +1,8 @@
+from graph_inference.graphs.tree import *
+from graph_inference.sim.sirsim import *
+from graph_inference.solver.greedysolver import *
+
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
